0dc9c0273a9816c0011e406c15735c7ae331fa01
unknown <atuliit444@gmail.com>
2024-01-04 19:56:59 +0530

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const dotenv=require("dotenv").config()
 
-const port =process.env.PORT|| 8000;
+const port =process.env.PORT|| 5001;
 const app=express()
 
 app.use("/api/contacts",require("./Routes/contactRoute"))
